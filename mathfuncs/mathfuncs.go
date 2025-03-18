3d10ca20bdc378d67052edd2925f53a4ef6c33cf
namcnab <nnekaxmcnab@gmail.com>
2025-03-18 14:16:19 -0700

    creates additional calculator functions

--- mathfuncs/mathfuncs.go
+++ mathfuncs/mathfuncs.go
@@ -1,5 +1,63 @@
 package mathfuncs
 
-func Add(a, b int) int {
+func AddTwo(a, b int) int {
 	return a + b
+}
+
+func SubtractTwo(a, b int) int {
+	return a - b
+}
+
+func MultiplyTwo(a, b int) int {
+	return a * b
+}
+
+func DivideTwo(a, b int) int {
+	if b == 0 {
+		return 0 // Handle division by zero
+	}
+	return a / b
+}
+
+func AddList[T []int | []float32 | []float64](numbers T) any {
+	var sum any
+
+	if len(numbers) == 0 {
+		return 0
+	}
+
+	switch nums := any(numbers).(type) {
+	case []int:
+		sum := 0
+		for _, num := range nums {
+			sum += num
+		}
+	case []float32:
+		sum := float32(0)
+		for _, num := range nums {
+			sum += num
+		}
+	case []float64:
+		sum := float64(0)
+		for _, num := range nums {
+			sum += num
+		}
+
+	default:
+		sum = 0
+		return "Unsupported type"
+	}
+
+	return sum
+}
+
+func SubtractList(numbers []int) int {
+	if len(numbers) == 0 {
+		return 0
+	}
+	result := numbers[0]
+	for _, num := range numbers[1:] {
+		result -= num
+	}
+	return result
 }
