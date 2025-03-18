64a0e987c24cd3866f0a89fc9ec6b0b533774410
namcnab <nnekaxmcnab@gmail.com>
2025-03-18 14:53:07 -0700

    adds additional routes to the gin engine

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/namcnab/calculator/mathfuncs"
@@ -14,20 +12,63 @@ func main() {
 	// Add CORS middleware
 	ginEngine.Use(cors.Default())
 
-	ginEngine.POST("/addtwo", AddHandler)
+	// Define routes
+	ginEngine.POST("/addtwo", AddTwoHandler)
+	ginEngine.POST("/addList", AddListHandler)
+	ginEngine.POST("/dividetwo", DivideTwoHandler)
+	ginEngine.POST("/multiplytwo", MultiplyTwoHandler)
+	ginEngine.POST("/subtracttwo", SubtractTwoHandler)
+
+	// Start the server on port 8080
 	ginEngine.Run(":8080")
-	fmt.Println(mathfuncs.AddTwo(1, 2))
 }
 
-func AddHandler(c *gin.Context) {
-	var input struct {
-		A int `json:"a"`
-		B int `json:"b"`
-	}
+func AddTwoHandler(c *gin.Context) {
+	var input mathfuncs.TwoNums
 	if err := c.BindJSON(&input); err != nil {
 		c.JSON(400, gin.H{"error": "Invalid input"})
 		return
 	}
 	result := mathfuncs.AddTwo(input.A, input.B)
+	c.JSON(200, gin.H{"result": result})
+}
+
+func AddListHandler(c *gin.Context) {
+	var input mathfuncs.ListOfNums
+	if err := c.BindJSON(&input); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid input"})
+		return
+	}
+	result := mathfuncs.AddList(input.List)
+	c.JSON(200, gin.H{"result": result})
+}
+
+func DivideTwoHandler(c *gin.Context) {
+	var input mathfuncs.TwoNums
+	if err := c.BindJSON(&input); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid input"})
+		return
+	}
+	result := mathfuncs.DivideTwo(input.A, input.B)
+	c.JSON(200, gin.H{"result": result})
+}
+
+func MultiplyTwoHandler(c *gin.Context) {
+	var input mathfuncs.TwoNums
+	if err := c.BindJSON(&input); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid input"})
+		return
+	}
+	result := mathfuncs.MultiplyTwo(input.A, input.B)
+	c.JSON(200, gin.H{"result": result})
+}
+
+func SubtractTwoHandler(c *gin.Context) {
+	var input mathfuncs.TwoNums
+	if err := c.BindJSON(&input); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid input"})
+		return
+	}
+	result := mathfuncs.SubtractTwo(input.A, input.B)
 	c.JSON(200, gin.H{"result": result})
 }
