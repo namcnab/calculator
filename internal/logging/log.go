b2a7502a9785b57ba02e58c443a1819bdb308659
namcnab <nnekaxmcnab@gmail.com>
2025-03-19 12:02:30 -0700

    defines application logging for api responses

--- internal/logging/log.go
+++ internal/logging/log.go
@@ -1,36 +1,66 @@
 package logging
 
 import (
-	"log"
 	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
-func SendLog(msg string, level string) {
-	// Open the log file. Create it if it doesn't exist; append if it does.
-	root := getModuleRoot()
-	localPath := root + "/logs/app.log"
-	//dockerPath := "/app/logs/app.log"
-	logFile, err := os.OpenFile(localPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal(err)
+type ResponseLogRecord struct {
+	ClientIP      string  `json:"client_ip"`
+	ServerIP      string  `json:"server_ip"`
+	Level         string  `json:"level"`
+	Status        int     `json:"status"`
+	HttpMethod    string  `json:"http_method"`
+	Message       string  `json:"message"`
+	SysErrMsg     string  `json:"sys_err_msg"`
+	OperationName string  `json:"operation_name"`
+	TimeStamp     string  `json:"time_stamp"`
+	TimeZone      string  `json:"time_zone"`
+	ResponseTime  float64 `json:"response_time"`
+}
+
+var logConfig = logrus.New()
+
+func InitLogger() {
+	// Set log format to JSON
+	logConfig.SetFormatter(&logrus.JSONFormatter{})
+
+	// Set log file path
+	//localPath := getModuleRoot() + "/logs/app.log"
+	dockerPath := "/app/logs/app.log"
+
+	// Write logs to a file
+	file, err := os.OpenFile(dockerPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err == nil {
+		logConfig.SetOutput(file)
+	} else {
+		logConfig.SetOutput(os.Stdout)
 	}
-	defer logFile.Close()
+}
 
-	// Set the output of the default logger to the file.
-	log.SetOutput(logFile)
+func LogResponse(fields ResponseLogRecord) {
+	entry := logConfig.WithFields(logrus.Fields{
+		"client_ip":      fields.ClientIP,
+		"server_ip":      fields.ServerIP,
+		"status":         fields.Status,
+		"http_method":    fields.HttpMethod,
+		"sys_err_msg":    fields.SysErrMsg,
+		"operation_name": fields.OperationName,
+		"time_stamp":     fields.TimeStamp,
+		"time_zone":      fields.TimeZone,
+		"response_time":  fields.ResponseTime,
+	})
 
-	// Log messages
-	switch level {
-	case "info":
-		log.Println("INFO: " + msg)
-	case "error":
-		log.Println("ERROR: " + msg)
-	case "debug":
-		log.Println("DEBUG: " + msg)
+	switch fields.Level {
+	case "INFO":
+		entry.Info(fields.Message)
+	case "ERROR":
+		entry.Error(fields.Message)
 	default:
-		log.Println("UNKNOWN LEVEL: " + msg)
+		entry.Debug(fields.Message)
 	}
 
 }
